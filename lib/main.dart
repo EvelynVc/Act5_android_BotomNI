eff1cd057670a9bf3c8a13ec6a0ef9455f909c6a
FlutLab.io <>
2021-05-17 19:07:14 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:eve/paginas_tabs/paginatb1.dart';
+import 'package:eve/paginas_tabs/paginatb2.dart';
+import 'package:eve/paginas_tabs/paginatb3.dart';
+
+void main() {
+  runApp(MaterialApp(
+      // Title
+      title: "Using Tabs",
+      debugShowCheckedModeBanner: false,
+      // Home
+      home: Mykasa()));
+} //fin de main
+
+class Mykasa extends StatefulWidget {
+  @override
+  MykasaState createState() => MykasaState();
+} //fin de Mikasa
+
+class MykasaState extends State<Mykasa> with SingleTickerProviderStateMixin {
+  TabController controller;
+  @override
+  void initState() {
+    super.initState();
+
+    controller = TabController(length: 3, vsync: this);
+  } //fin de iniciar estado
+
+  @override
+  void dispose() {
+    // Dispose of the Tab Controller
+    controller.dispose();
+    super.dispose();
+  } //fin de dispose
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Appbar
+      appBar: AppBar(
+        // Title
+        title: Text("Boton de navegacion inferior"),
+        // Set the background color of the App Bar
+        backgroundColor: Colors.black,
+      ),
+      // Set the TabBar view as the body of the Scaffold
+      body: TabBarView(
+        // Add tabs as widgets
+        children: <Widget>[
+          PrimerTab(),
+          SegundoTab(),
+          TercerTab()
+        ],
+        // set the controller
+        controller: controller,
+      ),
+      // Set the bottom navigation bar
+      bottomNavigationBar: Material(
+        // set the color of the bottom navigation bar
+        color: Colors.black,
+        // set the tab bar as the child of bottom navigation bar
+        child: TabBar(
+          tabs: <Tab>[
+            Tab(
+              // set icon to the tab
+              icon: Icon(Icons.favorite, color: Colors.red),
+            ),
+            Tab(
+              icon: Icon(Icons.adb, color: Colors.green),
+            ),
+            Tab(
+              icon: Icon(Icons.airport_shuttle, color: Colors.amber),
+            ),
+          ], //fin de tabs
+
+          controller: controller,
+        ), //fin de Tabar
+      ), //fin de bottom navigator inferior/material
+    ); //fin de scaffold
+  } //fin de widget
+} //fin de MikasaState
